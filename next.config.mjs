3e726079077ec70d770c3185788ec10a43ced52f
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2024-05-26 16:52:03 +0200

    agrement

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["127.0.0.1"],
-    },
-  };
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'maindoclinicsystem.pythonanywhere.com/',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'play.google.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'developer.apple.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_BASE_API: process.env.NEXT_PUBLIC_BASE_API,
+  },
+};
   
   export default nextConfig;
--- utils/variables.ts
+++ utils/variables.ts
@@ -1,1 +1,1 @@
-export const baseAPI = 'http://127.0.0.1:8000'
+export const baseAPI = process.env.NEXT_PUBLIC_BASE_API
